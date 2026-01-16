3af868eb1aea9d9199b9de5b717cfd4100e24592
Razi <Khanrazi002@gmail.com>
2026-01-16 20:51:09 +0530

    Fix prediction using vectorizers to prevent 500 error

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 import joblib
 import os
 
@@ -6,9 +6,14 @@ app = FastAPI(title="Customer Support Ticket Analyzer API")
 
 ISSUE_MODEL_PATH = os.path.join("models", "issue_model.pkl")
 URGENCY_MODEL_PATH = os.path.join("models", "urgency_model.pkl")
+ISSUE_VECTOR_PATH = os.path.join("models" , "issue_vectorizer.pkl")
+URGENCY_VECTOR_PATH = os.path.join("models","urgency_vectorizer.pkl")
 
 issue_model = joblib.load(ISSUE_MODEL_PATH)
 urgency_model = joblib.load(URGENCY_MODEL_PATH)
+issue_vectorizer = joblib.load(ISSUE_VECTOR_PATH)
+urgency_vectorizer = joblib.load(URGENCY_VECTOR_PATH)
+
 
 @app.get("/")
 def health_check():
@@ -16,11 +21,26 @@ def health_check():
 
 @app.post("/api/predict")
 def predict(issue: str):
-    issue_type = issue_model.predict([issue])[0]
-    urgency = urgency_model.predict([issue])[0]
+    try:
+        #getting input in the form of vectors
+        issue_vec= issue_vectorizer([issue])
+        urgency_vec = urgency_vectorizer([issue])
 
-    return {
-        "issue": issue,
-        "issue_type": issue_type,
-        "urgency": urgency
-    }
+
+        #prediction
+        issue_type = issue_model.predict([issue])[0]
+        urgency = urgency_model.predict([issue])[0]
+
+
+        return {
+            "issue": issue,
+            "issue_type": issue_type,
+            "urgency": urgency
+        }
+     
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Prediction failed: {str(e)}"
+        )
+
