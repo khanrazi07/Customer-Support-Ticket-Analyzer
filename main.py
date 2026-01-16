2bf70a2f5580a838b4647f0188e811ece2ed7c8f
Razi <Khanrazi002@gmail.com>
2026-01-16 21:40:43 +0530

    fixing main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , HTTPException
+from fastapi import FastAPI, HTTPException
 import joblib
 import os
 
@@ -6,14 +6,14 @@ app = FastAPI(title="Customer Support Ticket Analyzer API")
 
 ISSUE_MODEL_PATH = os.path.join("models", "issue_model.pkl")
 URGENCY_MODEL_PATH = os.path.join("models", "urgency_model.pkl")
-ISSUE_VECTOR_PATH = os.path.join("models" , "issue_vectorizer.pkl")
-URGENCY_VECTOR_PATH = os.path.join("models","urgency_vectorizer.pkl")
+ISSUE_VECTOR_PATH = os.path.join("models", "issue_vectorizer.pkl")
+URGENCY_VECTOR_PATH = os.path.join("models", "urgency_vectorizer.pkl")
 
+# Load models and vectorizers
 issue_model = joblib.load(ISSUE_MODEL_PATH)
 urgency_model = joblib.load(URGENCY_MODEL_PATH)
 issue_vectorizer = joblib.load(ISSUE_VECTOR_PATH)
 urgency_vectorizer = joblib.load(URGENCY_VECTOR_PATH)
-
 
 @app.get("/")
 def health_check():
@@ -22,25 +22,24 @@ def health_check():
 @app.post("/api/predict")
 def predict(issue: str):
     try:
-        #getting input in the form of vectors
-        issue_vec= issue_vectorizer.transform([issue])
-        urgency_vec = urgency_vectorizer.transform([issue])
+        issue_text = str(issue)
 
+        # Vectorize input
+        issue_vec = issue_vectorizer.transform([issue_text])
+        urgency_vec = urgency_vectorizer.transform([issue_text])
 
-        #prediction
-        issue_type = issue_model.predict([issue])[0]
-        urgency = urgency_model.predict([issue])[0]
-
+        # Predict using VECTORS (THIS WAS THE FIX)
+        issue_type = issue_model.predict(issue_vec)[0]
+        urgency = urgency_model.predict(urgency_vec)[0]
 
         return {
-            "issue": issue,
+            "issue": issue_text,
             "issue_type": issue_type,
             "urgency": urgency
         }
-     
+
     except Exception as e:
         raise HTTPException(
             status_code=500,
             detail=f"Prediction failed: {str(e)}"
         )
-
